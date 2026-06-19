10b5a4f263f37223c8b9c8f26314985e783f8e9a
Thiago Tolotti <thiagotolott@gmail.com>
2026-06-19 17:14:45 -0300

    adds upload image request

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
 @app.get("/")
 def ping():
     return {"message": "Service is alive!"}
+
+@app.post("/receipt")
+def upload_receipt(file: UploadFile):
+    print(f"Received file: {file.filename}")
+    
+    return {"message": f"File '{file.filename}' uploaded successfully!"}
