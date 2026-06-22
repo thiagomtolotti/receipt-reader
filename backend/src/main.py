84e2a67cfd1fe940d1b882e8619cd12c87bbd561
Thiago Tolotti <thiagotolott@gmail.com>
2026-06-22 17:31:17 -0300

    adds handling for invalid receipt

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from typing import Any
+
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from scripts.migrate import migrate
 
 from src.constants import DB_PATH
+from src.infra.ai_repository.gemini import InvalidReceiptError
 from src.presentation.receipt_router import ReceiptRouter
 
 app = FastAPI()
@@ -27,3 +31,13 @@ def ping():
 
 receipt_router = ReceiptRouter()
 app.include_router(receipt_router)
+
+
+def invalid_receipt_exception_handler(_: Any, exc: Exception):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={"detail": str(exc)},
+    )
+
+
+app.add_exception_handler(InvalidReceiptError, invalid_receipt_exception_handler)
