98c68508273dcee6feaa1021c985135de36b468a
Thiago Tolotti <thiagotolotti@gmail.com>
2026-06-22 00:01:17 -0300

    adds shadcn, adds table in home

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,5 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useQuery } from '@tanstack/react-query'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '#/components/ui/table'
 
 export const Route = createFileRoute('/')({ component: Home })
 
@@ -8,14 +16,35 @@ function Home() {
 
   return (
     <div className="p-8 py-16 w-full max-w-7xl mx-auto">
-      <h1 className="text-4xl font-bold mb-12">Leitor de notas fiscais</h1>
+      <h1 className="text-4xl font-bold mb-12">Receipt Reader</h1>
 
-      {data?.receipts?.map((receipt) =>
-        (receipt['total'] / 100).toLocaleString('pt-BR', {
-          style: 'currency',
-          currency: 'BRL',
-        }),
-      ) ?? 'Carregando...'}
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Date</TableHead>
+            <TableHead>Store Name</TableHead>
+            <TableHead>Total</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data?.receipts.map((receipt) => (
+            <TableRow key={receipt.id}>
+              <TableCell>
+                {new Date(receipt.date).toLocaleDateString('pt-BR')}
+              </TableCell>
+              <TableCell>{receipt.store_name}</TableCell>
+              <TableCell>
+                {(receipt.total / 100).toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })}
+              </TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   )
 }
